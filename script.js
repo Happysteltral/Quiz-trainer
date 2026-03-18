c74c916aa0c994c494c24615fc52a47d84f00510
Nicky <115946758+Happysteltral@users.noreply.github.com>
2026-03-18 21:18:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,56 @@ let vragen = [
         vraag: "Welke taal gebruiken we in de browser?",
         antwoorden: ["Python", "C#", "JavaScript", "Java"],
         correct: 2
+    },
+    {
+        vraag: "Wat is de hoofdstad van Frankrijk?",
+        antwoorden: ["Parijs", "Rome", "Madrid", "Berlijn"],
+        correct: 0
+    },
+    {
+        vraag: "Hoeveel bits zitten er in een byte?",
+        antwoorden: ["4", "8", "16", "32"],
+        correct: 1
+    },
+    {
+        vraag: "Welke HTML-tag gebruik je voor een link?",
+        antwoorden: ["<p>", "<a>", "<div>", "<link>"],
+        correct: 1
+    },
+    {
+        vraag: "Wat is de uitkomst van 10 / 2?",
+        antwoorden: ["2", "4", "5", "10"],
+        correct: 2
+    },
+    {
+        vraag: "Welke kleur krijg je door rood en blauw te mengen?",
+        antwoorden: ["Groen", "Paars", "Oranje", "Geel"],
+        correct: 1
+    },
+    {
+        vraag: "Waar staat CSS voor?",
+        antwoorden: [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style System",
+            "Colorful Style Sheets"
+        ],
+        correct: 1
+    },
+    {
+        vraag: "Welke lus gebruik je als je niet weet hoe vaak je moet herhalen?",
+        antwoorden: ["for", "while", "if", "switch"],
+        correct: 1
+    },
+    {
+        vraag: "Wat doet JavaScript voornamelijk?",
+        antwoorden: [
+            "Servers beheren",
+            "Webpagina’s interactief maken",
+            "Databases opslaan",
+            "Besturingssystemen maken"
+        ],
+        correct: 1
     }
 ];
 
